088ff562ad8051c8b5c3d6ed3928f507f52a5e4e
st-sathish <sathish.2009btech@gmail.com>
2018-08-11 22:24:14 +0530

    add cookie form

--- app/src/main/java/csr/capestart/com/fragments/AddCookieFragment.java
+++ app/src/main/java/csr/capestart/com/fragments/AddCookieFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import csr.capestart.com.R;
 import csr.capestart.com.extras.AppConstants;
 
-public class AddCookieFragment extends BaseFragment {
+public class AddCookieFragment extends BaseFragment implements View.OnClickListener {
+
+    EditText etExpiryDate;
 
     public AddCookieFragment() {
 
@@ -26,6 +29,20 @@ public class AddCookieFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mParentView = inflater.inflate(R.layout.fr_add_cookie, container, false);
+        etExpiryDate = mParentView.findViewById(R.id.input_expiry_date);
+        etExpiryDate.setOnClickListener(this);
         return mParentView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.input_expiry_date:
+                DatePickerFragment dFragment = new DatePickerFragment();
+                dFragment.show(getFragmentManager(), "Date Picker");
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/csr/capestart/com/fragments/DatePickerFragment.java
@@ -0,0 +1,27 @@
+package csr.capestart.com.fragments;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+}
